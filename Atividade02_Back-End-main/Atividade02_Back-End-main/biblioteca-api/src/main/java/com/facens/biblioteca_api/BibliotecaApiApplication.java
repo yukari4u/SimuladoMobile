d400352cffb772afe2861199c8b91853c43d0ee9
Aluno <aluno@facens.br>
2026-03-19 22:07:27 -0300

    add

---
+++ Atividade02_Back-End-main/Atividade02_Back-End-main/biblioteca-api/src/main/java/com/facens/biblioteca_api/BibliotecaApiApplication.java
@@ -0,0 +1,13 @@
+package com.facens.biblioteca_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaApiApplication.class, args);
+	}
+
+}
