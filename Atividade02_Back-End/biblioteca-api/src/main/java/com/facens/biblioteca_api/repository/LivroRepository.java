e578ceb97f9f3a1dc5928e2cc93f336ac98b1252
Aluno <aluno@facens.br>
2026-03-26 19:27:27 -0300

    att

---
+++ Atividade02_Back-End/biblioteca-api/src/main/java/com/facens/biblioteca_api/repository/LivroRepository.java
@@ -0,0 +1,5 @@
+package com.facens.biblioteca_api.repository;
+
+public class LivroRepository {
+    
+}
---
+++ Atividade02_Back-End/biblioteca-api/src/test/java/com/facens/biblioteca_api/BibliotecaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facens.biblioteca_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
