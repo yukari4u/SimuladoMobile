1167bbab2469e5ece29afcc1b1c55570bb9ffd41
yukari4u <mkato4@gmail.com>
2026-03-26 20:07:18 -0300

    Removendo pastas fantasmas e corrigindo estrutura

--- Atividade02_Back-End/biblioteca-api/src/main/java/com/facens/biblioteca_api/service/LivroService.java
+++ Atividade02_Back-End/biblioteca-api/src/main/java/com/facens/biblioteca_api/service/LivroService.java
@@ -1,7 +1,13 @@
 package com.facens.biblioteca_api.service;
 
-
+import com.facens.biblioteca_api.repository.LivroRepository;
 
 public class LivroService {
-    
+    private final LivroRepository livroRepository;
+
+    public LivroService(LivroRepository livroRepository){
+        this.livroRepository = livroRepository;
+    }
+
+
 }
